e9c216df91dba79bbd5bfac6e496a7ce66d71bcf
sebastian.lopez <sebastian.lopez@tred-solutions.com>
2023-04-29 21:24:37 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sebastian4/sl-go-logger-2
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,15 @@
+package slgologger2
+
+import "log"
+
+func LogInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func LogWarn(message string) {
+	log.Printf("WARN - %v", message)
+}
+
+func LogError(message string) {
+	log.Printf("ERROR - %v", message)
+}
